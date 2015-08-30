38f5112f28a996772c0df9a05261a97dd646a931
luckysahani <luckythegreat4@gmail.com>
2015-08-30 18:28:18 +0530

    1st part done

--- bnl.py
+++ bnl.py
@@ -1,24 +1,31 @@
 import os
+import time
 dimensions_skyline_list=[];
 window_maxsize=0;
 window_data=[];
 data_list=[];
 skylines=[];
 timestamp=0;
-with open('query1.txt') as f:
+file_counter=0;
+flag3=0;
+number_of_comparisions=0;
+with open('sample_query.txt') as f:
     temp_list = [[int(x) for x in line.split()] for line in f];
     dimensions_skyline_list = temp_list[0];
     window_maxsize = temp_list[1][0]
-with open('sample2.txt') as f:
-    data_list = [[int(x) for x in line.split()] for line in f];
+with open('sample_ant.txt') as f:
+    data_list = [[float(x) for x in line.split()] for line in f];
 
 def compare(element_1,element_2):
+	global number_of_comparisions;
+	number_of_comparisions=number_of_comparisions+1;
 	temp_list=[];
 	x=element_1[1];
 	y=element_2[1];
+	# print x,y,temp_list;
 	for i in dimensions_skyline_list:
 		temp_list.append(x[i]-y[i]);
-	# print x,y,temp_list;
+	
 	flag1=0;
 	flag2=0;
 	for elem in temp_list:
@@ -34,7 +41,9 @@ def compare(element_1,element_2):
 		return 1
 
 def find_skylines(sample_data):
-	global window_data,timestamp;
+	global window_data,timestamp,file_counter,flag3;
+	flag3=0;
+	all_skyline_before_time=0;
 	data_tuple_with_timestamp=[];
 	for data in sample_data:
 		timestamp=timestamp+1;
@@ -46,12 +55,9 @@ def find_skylines(sample_data):
 			topush_data=False;
 			flag1=0;
 			new_window_data=[];
-			# print "\n",window_data;
 			for i in xrange(0,len(window_data)): 
-				# print window_data,i,window_data[i];
 				element=window_data[i];
 				compare_result = compare(data,element);
-				# print compare_result;
 				if (compare_result == 1):
 					flag1=1;
 					topush_data=True;
@@ -68,38 +74,38 @@ def find_skylines(sample_data):
 					new_window_data.append(data);
 				else:
 					with open("skyline_temporary_file.txt", "a") as myfile:
-						# print window_data,data;
+						if(flag3==0):
+							all_skyline_before_time=data[0];
+							flag3=1;
 						for integer in data[1]:
 							myfile.write(str(integer));
 							myfile.write(" ");
 						myfile.write("\n");
 			window_data=new_window_data;
-			print window_data;
-	print "\nIteration Complete\n";
 	if(os.stat("skyline_temporary_file.txt").st_size == 0):
-		print "Done";
 		for data in window_data:
-			skylines.append(data);
+			skylines.append(data[1]);
 	else:
-		# print "Size=",os.stat("skyline_temporary_file.txt").st_size;
 		temp_data_list=[];
 		new_window_data=[];
 		with open('skyline_temporary_file.txt') as f:
-			temp_data_list = [[int(x) for x in line.split()] for line in f];
+			temp_data_list = [[float(x) for x in line.split()] for line in f];
 		fo = open("skyline_temporary_file.txt", "rw+");
 		fo.truncate();
 		for data in window_data:
-			# print temp_data_list[0][0] , data[0];
-			if(temp_data_list[0][0] > data[0]):
-				skylines.append(data[0]);
+			if(all_skyline_before_time > data[0]):
+				skylines.append(data[1]);
 			else:
 				new_window_data.append(data);
 		window_data=new_window_data;
-		print "File data=",temp_data_list,"\n";	
+		# print window_data;
+		# print "File data=",temp_data_list,"\n";	
 		find_skylines(temp_data_list);
-
+start_time=int(round(time.time() * 1000));
 find_skylines(data_list);
-print skylines;
-
-
+end_time=int(round(time.time() * 1000));
+total_time=end_time-start_time;
+print "Total running time =",total_time,"\n";
+print "Number of object-to-object comparisons = ",number_of_comparisions,"\n";
+print "Size of skyline set = ",len(skylines),"\n";
 
--- sampleinputgenerator.py
+++ sampleinputgenerator.py
@@ -1,3 +1,3 @@
 from random import randint
-for i in xrange(0,20):
-	print i, randint(1,9),randint(0,9),randint(0,9);
+for i in xrange(0,200):
+	print i, randint(1,9),randint(1,9),randint(1,9),randint(1,9),randint(1,9);
